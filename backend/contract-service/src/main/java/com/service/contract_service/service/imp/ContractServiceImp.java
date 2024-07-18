a9e73f08c90001cccebb549077fdd1504ae2d8f5
Dev-JeanCharles <jeancduasan@gmail.com>
2024-07-18 00:02:57 -0300

    :zap: perf: adicionando loggers no ContractServiceImp

--- backend/contract-service/src/main/java/com/service/contract_service/service/imp/ContractServiceImp.java
+++ backend/contract-service/src/main/java/com/service/contract_service/service/imp/ContractServiceImp.java
@@ -4,6 +4,8 @@ import com.service.contract_service.application.web.controllers.builder.Contract
 import com.service.contract_service.application.web.controllers.dto.responses.ContractResponse;
 import com.service.contract_service.domain.model.Contract;
 import com.service.contract_service.service.ContractService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.service.contract_service.repository.postgres.ContractRepository;
@@ -12,8 +14,11 @@ import com.service.contract_service.repository.postgres.dao.ContractDAO;
 @Service
 public class ContractServiceImp implements ContractService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ContractServiceImp.class);
+
     private final ContractRepository repository;
     private final ContractBuilder contractBuilder;
+
 
     @Autowired
     public ContractServiceImp(ContractRepository repository, ContractBuilder contractBuilder) {
@@ -23,11 +28,16 @@ public class ContractServiceImp implements ContractService {
 
     @Override
     public ContractResponse create(Contract contract) {
+        logger.info("[CREATE-CONTRACT]-[Service] Creating a new contract with personId: {}", contract.getPersonId());
 
         ContractDAO contractDAO = convertToContractDAO(contract);
+        logger.debug("[CREATE-CONTRACT-[Service] ContractDAO created: {}", contractDAO);
+
         ContractDAO savedContractDAO = repository.save(contractDAO);
+        logger.info("[CREATE-CONTRACT]-[Service] Contract saved with id: {}", savedContractDAO.getId());
 
         Contract savedContract = convertToContract(savedContractDAO);
+        logger.debug("[CREATE-CONTRACT]-[Service] Converted saved ContractDAO to Contract: {}", savedContract);
 
         return contractBuilder.toContractResponse(savedContract);
     }
